180c0aab47ac3f8b2d1ce46b18f55f22f5b26528
phpbae <rebch86@daum.net>
2019-04-02 23:26:23 +0900

    - 라우터 구성 - 스토어 구성 및 연동

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -7,6 +7,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+// state manage
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+
 const styles = {
     flexGrow: {
         flexGrow: 1,
@@ -21,7 +26,7 @@ class Footer extends Component {
                 <AppBar position="absolute" color="secondary" style={{bottom: 0, top: 'auto'}}>
                     <Toolbar>
                         <Typography variant="h6" color="inherit" className={this.props.classes.flexGrow}>
-                            footer
+                            스토어 : {this.props.text}
                         </Typography>
                     </Toolbar>
                 </AppBar>
@@ -30,8 +35,29 @@ class Footer extends Component {
     }
 }
 
+
+// store 안의 state 값을 props 로 연결
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        text: state.test.storeTest,
+    };
+};
+
+/*
+    액션 생성자를 사용하여 액션을 생성하고,
+    해당 액션을 dispatch 하는 함수를 만들은 후, 이를 props 로 연결해줍니다.
+*/
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleTest: () => {
+            dispatch(actions.test())
+        },
+    }
+};
+
 Footer.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withRouter(withStyles(styles)(Footer));
+export default withRouter(withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Footer)));
