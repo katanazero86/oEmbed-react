ee38d58e020c4494a68fa9f39a0c608a4806464a
phpbae <rebch86@daum.net>
2019-04-17 23:41:29 +0900

    - 라우터 구성 - 스토어 구성 및 연동

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 class App extends Component {
+
     render() {
         return (
-           <div>
-               app.js
+           <div style={{padding : '20px'}}>
+               나와라!
+               <Grid container spacing={16}>
+                   <Grid item xs={12}>
+                       <Paper style={{padding : '10px'}}>xs=12</Paper>
+                   </Grid>
+                   <Grid item xs={6}>
+                       <Paper style={{padding : '10px'}}>xs=6</Paper>
+                   </Grid>
+                   <Grid item xs={6}>
+                       <Paper style={{padding : '10px'}}>xs=6</Paper>
+                   </Grid>
+               </Grid>
            </div>
         );
     }
+
 }
 
+// App.propTypes = {
+//   classes : PropTypes.object.isRequired,
+// };
 
-export default App;
+
+export default (App);
