d5e1fcb133829681c93efebfd87d298e8a83c81b
phpbae <rebch86@daum.net>
2019-04-21 14:56:22 +0900

    - react hooks 및 카드 및 그리드 살짝 적용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,87 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 
-import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import {Search} from '@material-ui/icons';
 
-class App extends Component {
+import CardComponent from './component/CardComponent';
 
-    render() {
-        return (
-           <div style={{padding : '20px'}}>
-               나와라!
-               <Grid container spacing={16}>
-                   <Grid item xs={12}>
-                       <Paper style={{padding : '10px'}}>xs=12</Paper>
-                   </Grid>
-                   <Grid item xs={6}>
-                       <Paper style={{padding : '10px'}}>xs=6</Paper>
-                   </Grid>
-                   <Grid item xs={6}>
-                       <Paper style={{padding : '10px'}}>xs=6</Paper>
-                   </Grid>
-               </Grid>
-           </div>
-        );
-    }
 
-}
+const domainItems = [
+    {
+        value: 'test1',
+        label: '유튜브',
+    },
+    {
+        value: 'test2',
+        label: '인스타그램',
+    },
+];
 
-// App.propTypes = {
-//   classes : PropTypes.object.isRequired,
-// };
 
+const App = (props) => {
+
+    const [selectDomain, setSelectDomain] = useState('test1');
+
+
+    const changeSelectDomain = (event) => {
+        console.log(event.target.value);
+        setSelectDomain(event.target.value);
+    };
+
+
+    return (
+        <div style={{padding : '20px'}}>
+            <Grid container spacing={16}>
+                <Grid item xs={12}>
+                    <Paper style={{padding : '10px', display : 'flex', alignItems: 'center', minWidth: '360px'}}>
+                        <TextField
+                            select
+                            variant="outlined"
+                            label="검색 도메인 선택"
+                            value={selectDomain}
+                            onChange={changeSelectDomain}
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start" style={{width : '100%'}}>도메인</InputAdornment>,
+                            }}
+                            style={{width : '100%'}}
+                        >
+                            {domainItems.map(option => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper style={{padding : '10px', display : 'flex', alignItems: 'center', minWidth: '360px'}}>
+                        <InputBase placeholder="Search.." style={{flexGrow: 2}} />
+                        <IconButton aria-label="Search" >
+                            <Search/>
+                        </IconButton>
+                    </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper style={{padding : '10px'}}>
+                        <CardComponent/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper style={{padding : '10px'}}>xs=4</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper style={{padding : '10px'}}>xs=4</Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+
+};
 
 export default (App);
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import { withRouter } from 'react-router-dom'
-import PropTypes from 'prop-types';
+import React, {useState, useEffect} from 'react';
+import {withRouter} from 'react-router-dom'
+// import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -20,25 +20,58 @@ const styles = {
 };
 
 
-class Header extends Component {
-    render() {
-        return (
-            <div className={this.props.classes.root}>
-                <AppBar position="static" color="primary">
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit" className={this.props.classes.flexGrow} onClick={() => {this.props.history.push('/')}}>
-                            React 연습(+ material-ui)
-                        </Typography>
-                        <Button color="inherit" size="large" onClick={() => {this.props.history.push('/about')}}>About</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
-}
+// hooks 예제
+const Header = (props) => {
+    const [headerMsg, setHeaderMsg] = useState('React 연습(Header 는 Hooks 로 작성)');
 
-Header.propTypes = {
-    classes: PropTypes.object.isRequired,
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다.(Header)');
+        console.log(headerMsg);
+    });
+
+    const onClickHandler = () => {
+        // setHeaderMsg('');
+        props.history.push('/');
+    };
+
+
+    return (
+        <div className={props.classes.root}>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" className={props.classes.flexGrow} onClick={onClickHandler}>
+                        {headerMsg}
+                    </Typography>
+                    <Button color="inherit" size="large" onClick={() => {
+                        props.history.push('/about')
+                    }}>About</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 };
+
+
+// class Header extends Component {
+//     render() {
+//         return (
+//             <div className={this.props.classes.root}>
+//                 <AppBar position="static" color="primary">
+//                     <Toolbar>
+//                         <Typography variant="h6" color="inherit" className={this.props.classes.flexGrow} onClick={() => {this.props.history.push('/')}}>
+//                             React 연습(+ material-ui)
+//                         </Typography>
+//                         <Button color="inherit" size="large" onClick={() => {this.props.history.push('/about')}}>About</Button>
+//                     </Toolbar>
+//                 </AppBar>
+//             </div>
+//         );
+//     }
+// }
+
+
+// Header.propTypes = {
+//     classes: PropTypes.object.isRequired,
+// };
 
 export default withRouter(withStyles(styles)(Header));
