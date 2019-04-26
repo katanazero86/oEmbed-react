158a7a6c4d3bfd09ec481a35818840a761d19d32
kyle <kyle@freshcode.me>
2019-04-26 14:40:40 +0900

    [feature] oEmbed 기능 개발 날짜 : 2019-04-22 담당자 : 배창현 - axios 관련 부분 수정 및 UI 조금 수정 - list 랜더링 문제 이슈 - 리덕스 사가 작업 - 크로스 엔브 추가

--- src/about/About.js
+++ src/about/About.js
@@ -1,10 +1,55 @@
 import React, {Component} from 'react';
 
+import './About.css';
+
+
 class About extends Component {
 
     render() {
         return (
-            <div>제작자: 배님</div>
+            <div>
+                <h2>제작자: 배님</h2>
+                <div style={{padding : '10px'}}>
+                    <table className="table-class">
+                        <thead>
+                        <tr>
+                            <th>사용 기술</th>
+                            <th>비고</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>react</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>react router</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>redux</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>redux-saga</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>axios</td>
+                            <td>HTTP 통신 모듈</td>
+                        </tr>
+                        <tr>
+                            <td>cross-env</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>@material-ui</td>
+                            <td>@material-ui/core, @material-ui/icons</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
